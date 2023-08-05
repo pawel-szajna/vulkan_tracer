5e3ece4b040ddb38c4ff07b93e0a4010eaf9862c
Pawel Szajna <szajnapawel@gmail.com>
2023-08-05 19:36:04 +0200

    change debug scale

--- include/constants.hpp
+++ include/constants.hpp
@@ -5,7 +5,7 @@
 constexpr u32 renderWidth = 640;
 constexpr u32 renderHeight = 480;
 
-constexpr u32 samplesPerShaderPass = 1;
+constexpr u32 samplesPerShaderPass = 2;
 
 constexpr usize shapesLimit = 16;
 constexpr usize vectorsLimit = 32;
--- shaders/debug.glsl
+++ shaders/debug.glsl
@@ -1,25 +1,27 @@
+const uint debugScale = 5;
+
 void debug(uint x, uint y)
 {
-    if (y < 20 * 5)
+    if (y < debugScale * 5)
     {
-        switch ((y / 20) % 5)
+        switch ((y / debugScale) % 5)
         {
             case 0:
-            float k = float(1 + ((x / 20) % 4)) / 4;
+            float k = float(1 + ((x / debugScale) % 4)) / 4;
             results.pixels[x + inputs.width * y] = vec4(0, 0, k, 0);
             break;
             case 1:
-            uint n = (x / 20) % shapesLimit;
+            uint n = (x / debugScale) % shapesLimit;
             float m = float(inputs.shapes[n]) / float(shapesLimit);
             results.pixels[x + inputs.width * y] = vec4(m, 0, 0, 0);
             break;
             case 2:
-            uint q = (x / 20) % vectorsLimit;
+            uint q = (x / debugScale) % vectorsLimit;
             vec4 w = inputs.vectors[q];
             results.pixels[x + inputs.width * y] = w;
             break;
             case 3:
-            uint o = (x / 20) % scalarsLimit;
+            uint o = (x / debugScale) % scalarsLimit;
             float p = inputs.scalars[o];
             results.pixels[x + inputs.width * y] = vec4(0, p, 0, 0);
             break;
